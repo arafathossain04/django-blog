b3b52cb5910520aad5ca2ab7c6378dbce35f1a71
arafathossain04 <arafathossaintushar4@gmail.com>
2026-09-09 09:57:22 +0600

    added login and logout feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from blogs.models import *
 from assignments.models import *
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 #main work
 def home(request):
@@ -20,6 +22,7 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
+# register
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -34,3 +37,29 @@ def register(request):
         'form': form
     }
     return render(request, 'register.html', context)
+
+# login
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            print(form.errors)
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+# logout
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
